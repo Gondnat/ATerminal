624f7f562000df82f291cb4d1e5f5a2642503fd1
Odie <julykitt@gmail.com>
2017-05-12 09:52:24 +0800

    fix command not corrent with special character like emoji ☕️

---
+++ ATermial/HostTableViewController.swift
@@ -0,0 +1,90 @@
+//
+//  HostTableViewController.swift
+//  ATermial
+//
+//  Created by Daniel Tan on 11/05/2017.
+//  Copyright © 2017 Thnuth. All rights reserved.
+//
+
+import UIKit
+
+struct SSHServer {
+    var host:String!
+    var user:String!
+    var passwd:String!
+    
+}
+
+class HostTableViewController:  UITableViewController {
+    lazy var sshServers = [SSHServer]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sshServers.append(SSHServer(host: "192.168.8.100", user: "odie", passwd: "d"))
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    @IBAction func quickConnect(_ sender: UIBarButtonItem) {
+        let hostAddress = UIAlertController(title: "Connect to SSH", message: "Please input host address and port", preferredStyle: UIAlertControllerStyle.alert)
+        hostAddress.addTextField { (address:UITextField) in
+            address.resignFirstResponder()
+            address.keyboardType = .numbersAndPunctuation
+            address.placeholder =  "example: 192.168.0.1:22"
+            address.returnKeyType = .next
+        }
+        hostAddress.addTextField { (user:UITextField) in
+            user.returnKeyType = .next
+            user.keyboardType = .asciiCapable
+            user.placeholder = "User name"
+        }
+        hostAddress.addTextField { (passwd:UITextField) in
+            passwd.returnKeyType = .go
+            passwd.keyboardType = .asciiCapable
+            passwd.placeholder = "Password"
+        }
+        let connect = UIAlertAction(title: "Connect", style: UIAlertActionStyle.default) { (UIAlertAction) in
+            if let textFields = hostAddress.textFields {
+
+                self.showSSHView(host: textFields[0].text!, username: textFields[1].text!, passwd: textFields[2].text!)
+            }
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) { (UIAlertAction) in
+        }
+        hostAddress.addAction(connect)
+        hostAddress.addAction(cancel)
+        self.present(hostAddress, animated: true, completion: nil)
+    }
+    // MARK: tableview datasource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sshServers.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "hostCell")
+        if nil != cell {
+            if indexPath.row < sshServers.count {
+                let host = sshServers[indexPath.row]
+                cell?.textLabel?.text = String(format: "%@@%@", host.user, host.host)
+            }
+        }
+        return cell!
+    }
+    // MARK: tableview delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row < sshServers.count {
+            let sshServer = sshServers[indexPath.row]
+            showSSHView(host: sshServer.host, username: sshServer.user, passwd: sshServer.passwd)
+        }
+    }
+    
+    func showSSHView(host:String, username:String, passwd:String) {
+        if let SSHVC = self.storyboard?.instantiateViewController(withIdentifier: "SSHView") as? SSHViewController {
+            SSHVC.host = host
+            SSHVC.user = username
+            SSHVC.passwd = passwd
+            self.tableView.reloadData()
+            self.navigationController?.pushViewController(SSHVC, animated: true)
+        }
+    }
+}
--- ATermial/SSHViewController.swift
+++ ATermial/SSHViewController.swift
@@ -9,32 +9,198 @@
 import UIKit
 import NMSSH
 
-class SSHViewController: UIViewController {
-
+class SSHViewController: UIViewController, UITextViewDelegate, NMSSHSessionDelegate, NMSSHChannelDelegate {
+    @IBOutlet var textView: UITextView!
+    
+    private var lastCommand:String = ""
+    private var lastLinePrefix:String = "~$"
+    
+    private var session:NMSSHSession!
+    private var queue:DispatchQueue {
+        return DispatchQueue(label: "SSHVIEW.queue")
+    }
+    private var semaphore:DispatchSemaphore?
+    
+//    private var lastText:String = ""
+    
+    public var host:String!
+    public var user:String!
+    public var passwd:String!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if let session = NMSSHSession.connect(toHost: "192.168.2.4", withUsername: "odie") {
-        if session.isConnected {
-            if session.authenticate(byPassword: "d") {
-                do {
-                    let response = try session.channel.execute("ls -al")
-                    print(response)
-
-                } catch  {
-                    print(error)
-                }
-            }
-        }
-        }
+        self.session = NMSSHSession.connect(toHost: self.host, withUsername: self.user)
+        self.session.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
+        // keyboard notification
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: .UIKeyboardWillHide, object: nil)
+        
+        
+        queue.async {
+            if self.session.isConnected {
+                if !self.passwd.isEmpty{
+                    self.session.authenticate(byPassword: self.passwd)
+                } else {
+                    self.session.authenticateByKeyboardInteractive()
+                }
+                
+                if !self.session.isAuthorized {
+                    DispatchQueue.main.async(execute: {
+                        self.append("Authentication error")
+                        self.textView.isEditable = false
+                    })
+                } else {
+                    DispatchQueue.main.async(execute: {
+                        self.textView.isEditable = true
+                    })
+                    self.session.channel.delegate = self
+                    self.session.channel.requestPty = true
+                    self.session.channel.ptyTerminalType = .VT100
+                }
+                
+                do {
+                    try self.session.channel.startShell()
+                } catch  {
+                    DispatchQueue.main.async(execute: {
+                        self.textView.isEditable = false
+                        self.append(error.localizedDescription)
+                    })
+                }
+            } else {
+                DispatchQueue.main.async(execute: {
+                    self.append("can't connect to"+self.host)
+                })
+        }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    // MARK: IBAction
+    @IBAction func disconnect(_ sender: UIBarButtonItem) {
+        queue.async {
+            self.session.disconnect()
+        }
+    }
+    
+    // MARK:
+    func append(_ other:String) {
+        self.textView.text.append(other)
+        if let text = textView.text {
+            if let range = text.range(of: "\n", options: .backwards, range: text.startIndex..<text.index(before: text.endIndex)) {
+                lastLinePrefix = text[range.upperBound..<text.endIndex]
+            }
+        }
+    }
+    
+    func performCommand() {
+        if nil != semaphore {
+            passwd = self.lastCommand.substring(to: lastCommand.index(before: lastCommand.endIndex))
+            semaphore?.signal()
+        } else {
+            let command = self.lastCommand
+            queue.async(execute: { 
+                self.session.channel.write(command, error: nil, timeout: 10)
+            })
+            self.lastCommand = ""
+        }
+    }
+    
+    // MARK: NMSSHSession
+    func session(_ session: NMSSHSession!, keyboardInteractiveRequest request: String!) -> String! {
+        DispatchQueue.main.async(execute: {
+            self.append(request)
+            self.textView.isEditable = false
+        })
+        semaphore = DispatchSemaphore(value: 0)
+        semaphore?.wait()
+        semaphore = nil
+        return self.passwd
+    }
+    func session(_ session: NMSSHSession!, didDisconnectWithError error: Error!) {
+        DispatchQueue.main.async(execute: {
+            self.append(error.localizedDescription)
+            self.textView.isEditable = false
+        })
+    }
+    // MARK: NMSSHChannel
+    func channel(_ channel: NMSSHChannel!, didReadData message: String!) {
+        DispatchQueue.main.async(execute: {
+            self.append(message)
+        })
+    }
+    func channel(_ channel: NMSSHChannel!, didReadError error: String!) {
+        DispatchQueue.main.async(execute: {
+            self.append(error)
+        })
+    }
+    func channelShellDidClose(_ channel: NMSSHChannel!) {
+        DispatchQueue.main.async(execute: {
+            self.append("\nShell closed\n")
+            self.textView.isEditable = false
+        })
+    }
+    
+    // MARK: UITextView delegate
+    func textViewDidChange(_ textView: UITextView) {
+        textView.scrollRangeToVisible(NSMakeRange((textView.text as NSString).length-1, 1))
+    }
+    
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        if textView.selectedRange.location < (textView.text as NSString).length - (self.lastCommand as NSString).length - 1 {
+            textView.scrollRangeToVisible(NSMakeRange((textView.text as NSString).length, 0))
+        }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text.isEmpty {
+            if !self.lastCommand.isEmpty {
+                lastCommand.remove(at: lastCommand.index(before: lastCommand.endIndex))
+                
+                return true
+            } else {
+                return false
+            }
+        }
+        self.lastCommand.append(text)
+        if text == "\n" {
+            if let text = textView.text {
+                if let range = text.range(of: "\n", options: .backwards, range: text.startIndex..<text.index(before: text.endIndex)) {
+                    let lastLine = text[range.upperBound..<text.endIndex]
+                    lastCommand = lastLine[lastLinePrefix.endIndex..<lastLine.endIndex]
+                    lastCommand.append("\n")
+                }
+            }
 
-
+            performCommand()
+        }
+        return true
+    }
+    
+    // MARK: Notification
+    func keyboardWillShow(notification: NSNotification) {
+        let ownFrame = self.view.window!.convert(textView.frame, from: textView.superview)
+        if let userInfo = notification.userInfo {
+            let keyboardFrame = userInfo[UIKeyboardFrameEndUserInfoKey] as! CGRect
+            var coveredFrame = ownFrame.intersection(keyboardFrame)
+            coveredFrame = self.view.window!.convert(coveredFrame, to: textView.superview)
+            textView.contentInset = UIEdgeInsetsMake(textView.contentInset.top, 0, coveredFrame.size.height, 0)
+            textView.scrollIndicatorInsets = textView.contentInset
+        }
+    }
+    func keyboardWillHide(notification:NSNotification) {
+        textView.contentInset = UIEdgeInsetsMake(textView.contentInset.top, 0, 0, 0)
+        textView.scrollIndicatorInsets = textView.contentInset
+    }
 }
 
